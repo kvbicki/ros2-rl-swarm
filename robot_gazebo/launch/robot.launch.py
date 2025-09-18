81b8a38850e8af7fc2574f68da6b4109978916d9
Szymon Kubicki <szymon.kubicki342@gmail.com>
2025-09-18 22:21:29 +0200

    implemented robot_gazebo, gazebo launch is working

---
+++ robot_gazebo/launch/robot.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    robot_description_path = PathJoinSubstitution([
+        FindPackageShare("robot_description"),
+        "model",
+        "robot.xacro"
+    ])
+
+    robot_description = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        robot_description_path
+    ])
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=["gz", "sim", "-v4", "empty.sdf"],
+            output="screen"
+        ),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{"robot_description": robot_description}],
+            output="screen"
+        ),
+
+        Node(
+            package="ros_gz_sim",
+            executable="create",
+            arguments=["-world", "empty", "-topic", "robot_description"],
+            output="screen"
+        ),
+    ])
