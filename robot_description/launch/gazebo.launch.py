6f53fbf3c98f5a84815322a1f45ec228a981acc3
Szymon Kubicki <szymon.kubicki342@gmail.com>
2025-09-18 22:03:11 +0200

    rviz added, launch files still not working

--- robot_description/launch/gazebo.launch.py
+++ robot_description/launch/gazebo.launch.py
@@ -1,44 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
 import os
-from xacro import process_file
 
 def generate_launch_description():
-
-    pkg_robot_description = get_package_share_directory('robot_description')
-    urdf_file = os.path.join(pkg_robot_description, 'model', 'robot.xacro')
-
-    doc = process_file(urdf_file)
-    robot_desc = doc.toxml()
-
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-        )
-    )
-    
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'robot1'],
-        output='screen'
+    urdf_file = os.path.join(
+        get_package_share_directory('robot_description'),
+        'model',
+        'robot.xacro'
     )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_desc}]
-    )
-
-
+    robot_description = Command(['xacro ', urdf_file])
 
     return LaunchDescription([
-        gazebo_launch,
-        robot_state_publisher_node,
-        spawn_entity
+        # Publikacja URDF do parametru robot_description
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        # Gazebo
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            name='spawn_robot',
+            arguments=['-param', 'robot_description', '-ros-args'],
+            output='screen'
+        ),
     ])
