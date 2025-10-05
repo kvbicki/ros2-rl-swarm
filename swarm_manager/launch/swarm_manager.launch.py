54900fbc2066e28f05984e9befd23a26a593e446
Szymon Kubicki <szymon.kubicki342@gmail.com>
2025-10-05 15:52:23 +0200

    fix

--- swarm_manager/launch/swarm_manager.launch.py
+++ swarm_manager/launch/swarm_manager.launch.py
@@ -5,19 +5,19 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='swarm_manager',
-            executable='swarm_manager_node',
+            executable='swarm_manager_node.py',
             name='swarm_manager_node',
             output='screen'
         ),
         Node(
             package='swarm_manager',
-            executable='rl_agent',
+            executable='rl_agent.py',
             name='rl_agent',
             output='screen'
         ),
         Node(
             package='swarm_manager',
-            executable='swarm_env',
+            executable='swarm_env.py',
             name='swarm_env',
             output='screen'
         )
